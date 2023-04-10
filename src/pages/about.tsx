46b21293b3a29174c79899ef8c512c72cb82bcad
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-11 01:51:25 +0200

    Added simple navigation

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,16 @@
+import Link from "next/link";
+
 export default function AboutPage() {
   return (
     <main>
+      <ul>
+        <li>
+          <Link href="/">Clothify</Link>
+        </li>
+        <li>
+          <Link href="/about">About Us</Link>
+        </li>
+      </ul>
       <h1>About</h1>
     </main>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
     <main>
+      <ul>
+        <li>
+          <Link href="/">Clothify</Link>
+        </li>
+        <li>
+          <Link href="/about">About Us</Link>
+        </li>
+      </ul>
       <h1>Clothify</h1>
     </main>
   );
