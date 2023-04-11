7cbb656d72c3894bb3e87ef6e78cfca14e972531
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-11 14:55:53 +0200

    Styled NavBar

--- src/components/navbar/navbar.styles.ts
+++ src/components/navbar/navbar.styles.ts
@@ -1,10 +1,30 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.nav`
+export const OuterWrapper = styled.nav`
+    width: 100%;
+    height: 8rem;
+    font-weight: 500;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 10%;
+`
+
+export const Wrapper = styled.ul`
+    display: flex;
+    gap: 1.5rem;
+    list-style: none;
     color: ${({ theme }) => theme.dark};
     font-size: ${({ theme }) => theme.font.size.s};
-    font-weight: bold;
     ${({ theme }) => theme.mq.desktop} {
-        font-size: ${({ theme }) => theme.font.size.m};
+        font-size: ${({ theme }) => theme.font.size.s};
     }
+`
+
+export const CartWrapper = styled.span`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    font-size: ${({ theme }) => theme.font.size.xs};
 `
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,17 +1,28 @@
 import Link from "next/link";
-import { Wrapper } from "./navbar.styles";
+import Image from "next/image";
+import { OuterWrapper, Wrapper, CartWrapper } from "./navbar.styles";
 
 export default function NavBar() {
   return (
-    <Wrapper>
-      <ul>
+    <OuterWrapper>
+      <Wrapper>
         <li>
           <Link href="/">Clothify</Link>
         </li>
         <li>
           <Link href="/about">About Us</Link>
         </li>
-      </ul>
-    </Wrapper>
+      </Wrapper>
+      <CartWrapper>
+        <Image
+          priority
+          height={22}
+          width={22}
+          src="/cart.svg"
+          alt="Cart Iton"
+        />
+        <p>Cart</p>
+      </CartWrapper>
+    </OuterWrapper>
   );
 }
