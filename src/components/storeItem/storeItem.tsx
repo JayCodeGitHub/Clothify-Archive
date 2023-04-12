b5e72db9e3d3b3677f2e35ddc9f0cb7f123d0b7b
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-12 02:16:20 +0200

    Refactored Store Item and added content

--- src/components/storeItem/storeItem.tsx
+++ src/components/storeItem/storeItem.tsx
@@ -1,18 +1,24 @@
 import { Wrapper, StyledImage, Title, Price } from "./storeItem.styles";
 
-export default function StoreItem() {
+interface StoreItemProps {
+  title: string;
+  img: string;
+  price: string;
+}
+
+export default function StoreItem({ title, img, price }: StoreItemProps) {
   return (
     <Wrapper href="/">
       <StyledImage
-        src="/t-shirt.jpeg"
+        src={img}
         alt="Picture of the author"
         width={500}
         height={500}
         blurDataURL="data:..."
         placeholder="blur"
       />
-      <Title>T-Shirt</Title>
-      <Price>12$</Price>
+      <Title>{title}</Title>
+      <Price>{price}</Price>
     </Wrapper>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,22 +4,16 @@ import StoreItem from "@/components/storeItem/storeItem";
 export default function HomePage() {
   return (
     <Wrapper>
-      <StoreItem />
-      <StoreItem />
-      <StoreItem />
-      <StoreItem />
-      <StoreItem />
-      <StoreItem />
-      <StoreItem />
-      <StoreItem />
-      <StoreItem />
-      <StoreItem />
-      <StoreItem />
-      <StoreItem />
-      <StoreItem />
-      <StoreItem />
-      <StoreItem />
-      <StoreItem />
+      <StoreItem title="T-Shirt" img="/items/t-shirt.jpeg" price="12$" />
+      <StoreItem title="Red Hat" img="/items/hat.jpeg" price="20$" />
+      <StoreItem title="Jacket" img="/items/jacket.jpeg" price="45$" />
+      <StoreItem
+        title="Jean Jacket"
+        img="/items/jean-jacket.jpeg"
+        price="40$"
+      />
+      <StoreItem title="Sweatshirt" img="/items/sweatshirt.jpeg" price="30$" />
+      <StoreItem title="Trousers" img="/items/trousers.jpeg" price="20$" />
     </Wrapper>
   );
 }
