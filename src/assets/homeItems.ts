dae175165cc80ae5b0b75793213766810ecb18ab
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-12 02:25:41 +0200

    Moved Home page Items to separate file

---
+++ src/assets/homeItems.ts
@@ -0,0 +1,32 @@
+export const HomeItems = [
+    {
+        title: "T-Shirt",
+        img: "/items/t-shirt.jpeg",
+        price: "12$"
+    },
+    {
+        title: "Red Hat",
+        img: "/items/hat.jpeg",
+        price: "20$"
+    },
+    {
+        title: "Jacket",
+        img: "/items/jacket.jpeg",
+        price: "45$"
+    },
+    {
+        title: "Jean Jacket",
+        img: "/items/jean-jacket.jpeg",
+        price: "40$"
+    },
+    {
+        title: "Sweatshirt",
+        img: "/items/sweatshirt.jpeg",
+        price: "30$"
+    },
+    {
+        title: "Trousers",
+        img: "/items/trousers.jpeg",
+        price: "20$"
+    },
+]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,13 @@
 import { Wrapper } from "@/styles/pages/home.styles";
 import StoreItem from "@/components/storeItem/storeItem";
+import { HomeItems } from "@/assets/homeItems";
 
 export default function HomePage() {
   return (
     <Wrapper>
-      <StoreItem title="T-Shirt" img="/items/t-shirt.jpeg" price="12$" />
-      <StoreItem title="Red Hat" img="/items/hat.jpeg" price="20$" />
-      <StoreItem title="Jacket" img="/items/jacket.jpeg" price="45$" />
-      <StoreItem
-        title="Jean Jacket"
-        img="/items/jean-jacket.jpeg"
-        price="40$"
-      />
-      <StoreItem title="Sweatshirt" img="/items/sweatshirt.jpeg" price="30$" />
-      <StoreItem title="Trousers" img="/items/trousers.jpeg" price="20$" />
+      {HomeItems.map(({ title, img, price }) => (
+        <StoreItem title={title} img={img} price={price} key={title} />
+      ))}
     </Wrapper>
   );
 }
