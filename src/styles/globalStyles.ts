23cbc827a8836c545b987bf212a9417eeb1f3c5a
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-22 01:07:44 +0200

    Fix styles loading issue

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,5 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
 export const GlobalStyle = createGlobalStyle`
     *, *::before, *::after {
         padding: 0;
@@ -8,12 +12,12 @@ export const GlobalStyle = createGlobalStyle`
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
     }
-
     html,
     body {
         max-width: 100vw;
         overflow-x: hidden;
         background-color: #ECE9EA;
+        font-family: ${inter.style.fontFamily};
     }
 
     a {
