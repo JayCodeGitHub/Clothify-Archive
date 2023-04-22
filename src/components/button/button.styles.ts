fd66f8af49a3d0cd5d5b8cd142873929b3190217
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-22 21:16:20 +0200

    Added button hover animation

--- src/components/button/button.styles.ts
+++ src/components/button/button.styles.ts
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-     background-color: ${({ theme }) => theme.primary};
+    &:hover {
+        background-color: transparent;
+        color: ${({ theme }) => theme.dark};
+        border: solid 3px ${({ theme }) => theme.primary};
+    }
+    background-color: ${({ theme }) => theme.primary};
     color: ${({ theme }) => theme.light};
     font-size: ${({ theme }) => theme.font.size.xs};
     font-weight: 600;
@@ -9,6 +14,9 @@ export const Button = styled.button`
     width: 80%;
     border: none;
     padding: 1rem 0;
+    border: solid 3px transparent;
+    transition: all 0.2s ease-in-out;
+    cursor: pointer;
 `
 
 export default Button
