7f8f37b24c57bc5aaab7678b4500b64bc18b74ca
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-23 15:12:15 +0200

    Added intro animation to alert

--- src/components/alert/alert.styles.ts
+++ src/components/alert/alert.styles.ts
@@ -1,4 +1,15 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const intro = keyframes`
+  from {
+    opacity: 0;
+    transform: translate(-50%, -20px);
+  }
+  to {
+    opacity: 1;
+    transform: translate(-50%, 0px);
+  }
+`;
 
 export const Wrapper = styled.aside`
     position: fixed;
@@ -11,8 +22,9 @@ export const Wrapper = styled.aside`
     gap: 1rem;
     top: 2rem;
     left: 50%;
-    transform: translate(-50%);
+    transform: translateX(-50%);
     font-size: ${({ theme }) => theme.font.size.s};
+    animation: ${intro} 0.2s linear;
 `
 
 export const IconWrapper = styled.span`
