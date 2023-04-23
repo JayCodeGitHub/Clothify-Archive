5baba02a59cd0499269f4bca3457d7086389c6b4
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-23 19:59:20 +0200

    Styled purchase site

--- src/pages/purchase.tsx
+++ src/pages/purchase.tsx
@@ -1,9 +1,32 @@
-import { Wrapper } from "@/styles/pages/purchase.styles";
+import {
+  Wrapper,
+  CartSection,
+  Title,
+  FormSection,
+} from "@/styles/pages/purchase.styles";
+import { useCart } from "@/hooks/useCart";
+import CartItem from "@/components/cartItem/cartItem";
 
 export default function PurchasePage() {
+  const { cart } = useCart();
   return (
     <Wrapper>
-      <h1>Purchase</h1>
+      <CartSection>
+        <Title>Your order</Title>
+        {cart.map((item) => (
+          <CartItem
+            key={item.id}
+            id={item.id}
+            img={item.img}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+          />
+        ))}
+      </CartSection>
+      <FormSection>
+        <Title>Payment</Title>
+      </FormSection>
     </Wrapper>
   );
 }
--- src/styles/pages/purchase.styles.ts
+++ src/styles/pages/purchase.styles.ts
@@ -2,5 +2,34 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.main`
     width: 100%;
-    color: red;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    ${({ theme }) => theme.mq.desktop} {
+        flex-direction: row;
+    }
+`
+
+export const CartSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 1.5rem;
+    padding: 1.5rem 0;
+    ${({ theme }) => theme.mq.desktop} {
+        width: 35rem;
+    }
+`
+
+export const Title = styled.h1`
+    align-self: flex-start;
+    padding-left: 2rem;
+    font-size: ${({ theme }) => theme.font.size.m};
+`
+
+export const FormSection = styled.form`
+    width: 35rem;
+    height: 40rem;
+    padding: 1.5rem 0;
 `
