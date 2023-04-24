30bf52abf3ca1b6890fb0a3b692a1f6a88c71361
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-24 15:30:53 +0200

    Added first test and setup tests

--- src/__tests__/intex.test.tsx
+++ src/__tests__/intex.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { ThemeProvider } from "styled-components";
 import Home from "@/pages/index";
 import { theme } from "@/styles/theme";
