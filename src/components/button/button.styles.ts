3d07d606b0ec69d328daa4dc7d1ebae4c4f7f07c
JayCode Developer <JayCode.contakt@gmail.com>
2023-04-29 13:27:02 +0200

    Refactored Button

--- src/components/button/button.styles.ts
+++ src/components/button/button.styles.ts
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-    background-color: ${({ theme }) => theme.primary};
-    color: ${({ theme }) => theme.light};
-    font-size: ${({ theme }) => theme.font.size.xs};
-    font-weight: 600;
-    border-radius: 0.5rem;
     width: 80%;
     padding: 1rem 0;
+    font-weight: 600;
+    font-size: ${({ theme }) => theme.font.size.xs};
+    color: ${({ theme }) => theme.light};
+    cursor: pointer;
+    border-radius: 0.5rem;
+    background-color: ${({ theme }) => theme.primary};
     border: solid 3px transparent;
     transition: all 0.2s ease-in-out;
-    cursor: pointer;
     &:hover {
         background-color: transparent;
         color: ${({ theme }) => theme.dark};
